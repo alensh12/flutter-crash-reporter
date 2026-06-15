12d1cde14421c1c83a70a3bc03e313963ef3b37a
alensh12 <alensh.am12@gmail.com>
2026-06-15 15:16:27 +0545

    fix route configuration

--- api/index.js
+++ api/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("🚀 Crash Reporter Backend is Running!");
+});
 
 app.post('/crashes', (req, res) => {
   try {
     const crash = req.body;
     
-    console.log("🚨 CRASH REPORT RECEIVED");
+    console.log("🚨 NEW CRASH REPORT:");
     console.log(JSON.stringify(crash, null, 2));
 
     res.status(200).json({ 
